755806e5e1824b5df588173e5697124676614544
alexandra-ispas <aispas33@gmal.com>
2022-06-27 23:02:14 +0300

    day7

---
+++ src/Day7/Part2/Solver.scala
@@ -0,0 +1,29 @@
+package Day7.Part2
+
+import scala.io.{BufferedSource, Source}
+import scala.math.abs
+
+object Solver {
+  def main(args: Array[String]): Unit = {
+    val filename = "/Users/alexandraispas/Desktop/aoc-2021/src/Day7/Part1/input.txt"
+    val input: BufferedSource = Source.fromFile(filename)
+
+    val nrs = input.getLines().toArray
+    var positions: Array[Int] = nrs(0).split(',').map(_.toInt)
+
+    var average_pos: Double = positions.sum / positions.length
+
+    var min_fuel = Int.MaxValue
+
+    for (i <- positions.min until (average_pos.toInt + 1)) {
+      var p = 0
+      for (j <- positions) {
+        p += abs(i - j) * (abs(i - j) + 1) / 2
+      }
+      if (p < min_fuel)
+        min_fuel = p
+    }
+
+    println(min_fuel)
+  }
+}
